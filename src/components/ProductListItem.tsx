ab33e227a1fb337951b865bdb882f2e40d83fbf4
GokayDemir <gokaydemirpc@gmail.com>
2024-06-30 13:19:56 +0300

    Type safe component and path aliases implement

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,51 @@
+import Colors from "@/constants/Colors";
+import { Text, View, Image, StyleSheet } from "react-native";
+import { Product } from "src/types";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri:
+            product.image ||
+            "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+        }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontWeight: "600",
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+});
+
+export default ProductListItem;
